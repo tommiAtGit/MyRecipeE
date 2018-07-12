1d82cea53ada4948146f2a1bda4af213e060f38d
Tommi Korteikko <Tommi Korteikko>
2018-07-12 15:53:44 +0300

    Implementation of MealService

--- myRecipeApplication/src/main/java/com/myProjects/myRecipe/application/controler/MealControler.java
+++ myRecipeApplication/src/main/java/com/myProjects/myRecipe/application/controler/MealControler.java
@@ -8,63 +8,93 @@ import com.myProjects.myRecipe.domain.Ingredient;
 import com.myProjects.myRecipe.domain.Meal;
 import com.myProjects.myRecipe.domain.Recipe;
 import com.myProjects.myRecipe.domain.RecipeItem;
+import com.myProjects.myRecipe.repository.dao.MealServiceDAO;
 import com.myProjects.myRecipe.repository.dao.RecipeServiceDAO;
 import com.myProjects.myRecipe.repository.dao.impl.MealServiceDAOImpl;
 import com.myProjects.myRecipe.repository.dao.impl.RecipeServiceDAOImpl;
 
 public class MealControler {
 
-	private Meal myMeal = null;
-	private RecipeServiceDAOImpl mealService = null;
+	
+	private MealServiceDAO mealService = null;
 
-private RecipeServiceDAO recipeService = null;
+
 	
 	public MealControler(String persistenceUnitName) {
 		mealService  = new MealServiceDAOImpl(persistenceUnitName);
+	
 	}
 	
-	public Meal constructMeal(String name, int quatity ){
-		myMeal.setName(name);
-		myMeal.setQuatity(quatity);
-		
-		return myMeal;
-	}
-	public Meal addRecipe(Recipe res){
-		if (myMeal == null){
-			List<Recipe> recipesForNew = new ArrayList<Recipe>();
-			recipesForNew.add(res);
-			
-		}
-		else{
-			List <Recipe> recipes =  myMeal.getRecipe();
-			recipes.add(res);
-			//myMeal.setRecipe(recipes);
-		}
-		return myMeal;
-	}
-	
+	/**
+	 * Save new meal to database
+	 * @param meal
+	 * @return
+	 */
 	public Meal saveMeal(Meal meal ){
 
 		Meal savedMeal = null;
 		if (meal != null){
-			mealService.save(meal);
-			savedMeal = mealService.getMealWithName(meal);
+			savedMeal = mealService.saveMeal(meal);
 			return savedMeal;
+		}
+		else{
+			return null;
+		}	
+	}
+	
+	public List<Meal>fetshMelas(){
+		List<Meal> meals = mealService.fetchListOf();
+		return meals;
+	}
+
+		
+	/**
+	 * Delete selected meal
+	 * @param meal
+	 */
+	public void deleteMeal(Meal meal) {
+		if (meal != null) {
+			mealService.deleteMeal(meal);
+		}
+		
+	}
+
+	/**
+	 * Add new recipe to excising meal
+	 * @param meal
+	 * @param resipe
+	 * @return
+	 */
+	public Meal addNewRecipeToMeal(Meal meal, Recipe resipe){
+		Meal me = null;
+		
+		if ((meal != null) && (resipe != null)){
+			meal.getRecipe().add(resipe);
+			me = mealService.updateMeal(meal);
+			return me;
 		}
 		else{
 			return null;
 		}
 		
 	}
-	public void calculateMacros(Meal meal){
-		List <Recipe> recipes =  meal.getRecipe();
-		List <RecipeItem> recipeItems = recipes.get(0).getRecipeItems();
-		
-		
-		Ingredient ingredient = recipeItems.get(0).getIngredient();
-		double incredientQantity = recipeItems.get(0).getQuantity();
-		ingredient.getCarbonHydrates();
-		
-		
+	
+	/**
+	 * Remove recipe from meal
+	 * @param meal
+	 * @param recipe
+	 * @return
+	 */
+	public Meal removeRecipeFromMeal(Meal meal, Recipe recipe) {
+		Meal me = null;
+		if ((meal != null) && (recipe != null)){
+			meal.getRecipe().remove(recipe);
+			me = mealService.updateMeal(meal);
+			return me;
+		}
+		else{
+			return null;
+		}
 	}
+	
 }
